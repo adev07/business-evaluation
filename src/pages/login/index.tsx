dd6a4a99c89b3c48aac9fbcde36532b059d9b390
adev07 <adityaanand1245@gmail.com>
2024-10-10 19:49:45 +0530

    Not logged in funtionality

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -27,6 +27,14 @@ const Login = () => {
 
   return (
     <div className="flex flex-col min-h-screen bg-[#FAFAFA] px-4 sm:px-6 md:px-8">
+      <div className='ml-auto'>
+        <button
+          onClick={() => navigate("/add-business")}
+          className="w-[110px] mt-[24px] sm:mt-[30px] py-3 bg-[#3B37FF] text-white font-medium rounded-[8px] sm:rounded-[10px] hover:bg-blue-700 transition-colors"
+        >
+          Skip
+        </button>
+      </div>
       <ToastContainer />
       <div className="flex-grow flex flex-col items-center justify-center">
         <div className="w-full max-w-md">
